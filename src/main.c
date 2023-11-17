172ff930a84a81bac80035bb440079ffe824589f
iotcat <i@iotcat.me>
2023-11-17 22:49:50 +0000

    Fix collision detection in protothread_anim

--- src/main.c
+++ src/main.c
@@ -511,8 +511,9 @@ static PT_THREAD (protothread_anim(struct pt *pt))
 
       if(multfix15((a.x - b.x), (a.x - b.x)) + multfix15((a.y - b.y), (a.y - b.y)) < multfix15((a.radius + b.radius), (a.radius + b.radius))){
         // collide_function(&a, &b);
-      } else {
         move_balls(&a, &b);
+      } else {
+        
       }
       
       // for (int i = 0; i < NUM_OF_BOIDS_ON_CORE0; i++){
