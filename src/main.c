39ca11845d794024dcf3e1559d75a3be421d4471
yl2924 <yl2924@cornell.edu>
2023-12-05 22:53:17 +0000

    Fix bounce function to remove unnecessary bit shift

--- src/main.c
+++ src/main.c
@@ -300,7 +300,7 @@ void collide_function(ball* a, ball* b){
 void bounce_function(ball* b){
   if(hitBottom(b->y + b->type->radius)){
     b->y = int2fix15(BOX_BOTTOM) - b->type->radius;
-    b->vy = -b->vy >> 2;
+    b->vy = -b->vy;
     // b->vy = 0;
   }
   if(hitLeft(b->x - b->type->radius)){
