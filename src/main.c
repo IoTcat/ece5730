8b36034a507156c5d029add81000c7f8cb199ddb
yl2924 <yl2924@cornell.edu>
2023-12-05 22:05:49 +0000

    Commented out left and right boundary checks in bounce_function()

--- src/main.c
+++ src/main.c
@@ -286,14 +286,14 @@ void bounce_function(ball* b){
     // b->vy = -b->vy >> 2;
     b->vy = 0;
   }
-  if(hitLeft(b->x - b->type->radius)){
-    b->x = int2fix15(BOX_LEFT) + b->type->radius;
-    b->vx = -b->vx >> 2;
-  }
-  if(hitRight(b->x + b->type->radius)){
-    b->x = int2fix15(BOX_RIGHT) - b->type->radius;
-    b->vx = -b->vx >> 2;
-  }
+  // if(hitLeft(b->x - b->type->radius)){
+  //   b->x = int2fix15(BOX_LEFT) + b->type->radius;
+  //   b->vx = -b->vx >> 2;
+  // }
+  // if(hitRight(b->x + b->type->radius)){
+  //   b->x = int2fix15(BOX_RIGHT) - b->type->radius;
+  //   b->vx = -b->vx >> 2;
+  // }
 }
 
 void move_balls(ball* b){
