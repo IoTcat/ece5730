9e653584bb8db00a740790970fce9827e723a38b
iotcat <i@iotcat.me>
2023-12-07 20:45:47 +0000

    Fix drawBall function to use fillCircle instead of drawCircle

--- src/components/ball.c
+++ src/components/ball.c
@@ -57,8 +57,8 @@ void initBall(ball* a, fix15 init_x, ball_type* type){
 
 // Draw the ball
 void drawBall(ball* a, char color){
-  drawCircle(fix2int15(a->fx), fix2int15(a->fy), fix2int15(a->type->radius), BLACK);
-  drawCircle(fix2int15(a->x), fix2int15(a->y), fix2int15(a->type->radius), color);
+  fillCircle(fix2int15(a->fx), fix2int15(a->fy), fix2int15(a->type->radius), BLACK);
+  fillCircle(fix2int15(a->x), fix2int15(a->y), fix2int15(a->type->radius), color);
   a->fx = a->x;
   a->fy = a->y;
 }
