ca1a7608f4f7f161740f2b3439dfba60c2eb55b6
yl2924 <yl2924@cornell.edu>
2023-12-07 21:16:29 +0000

    Fix bug in login functionality

---
+++ src/components/gpio.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "config.h"
+// Include protothreads
+#include "lib/pt_cornell_rp2040_v1.h"
+
+enum joystick_direction {UP, DOWN, LEFT, RIGHT};
+
+
+//create a  int array with size 4
+int prev_joystick_state[4] = {0, 0, 0, 0}; //UP, DOWN, LEFT, RIGHT
+
+//return 1 if gpio rising edge is detected
+int gpio_edge(enum joystick_direction c){
+    if(prev_joystick_state[c] == 0 && gpio_get(JOSTICK_UP) == 1){
+        return 1;
+    }
+    if(prev_joystick_state[c] == 0 && gpio_get(JOSTICK_DOWN) == 1){
+        return 1;
+    }
+    if(prev_joystick_state[c] == 0 && gpio_get(JOSTICK_LEFT) == 1){
+        return 1;
+    }
+    if(prev_joystick_state[c] == 0 && gpio_get(JOSTICK_RIGHT) == 1){
+        return 1;
+    }
+    prev_joystick_state[c] = gpio_get(JOSTICK_UP);
+    prev_joystick_state[c] = gpio_get(JOSTICK_DOWN);
+    prev_joystick_state[c] = gpio_get(JOSTICK_LEFT);
+    prev_joystick_state[c] = gpio_get(JOSTICK_RIGHT);
+    return 0;
+}
+
+int gpio_value(enum joystick_direction c){
+    if(c == UP){
+        return gpio_get(JOSTICK_UP);
+    }
+    if(c == DOWN){
+        return gpio_get(JOSTICK_DOWN);
+    }
+    if(c == LEFT){
+        return gpio_get(JOSTICK_LEFT);
+    }
+    if(c == RIGHT){
+        return gpio_get(JOSTICK_RIGHT);
+    }
+    return 0;
+}
+
+
+
+
+
+
