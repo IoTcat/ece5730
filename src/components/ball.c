9e6e476ef4e2c2044f5a299667adf4c39ef841ec
iotcat <i@iotcat.me>
2023-12-07 21:23:07 +0000

    Refactor drawBall function to draw concentric circles

--- src/components/ball.c
+++ src/components/ball.c
@@ -57,10 +57,10 @@ void initBall(ball* a, fix15 init_x, ball_type* type){
 
 // Draw the ball
 void drawBall(ball* a, char color){
-  drawCircle(fix2int15(a->fx), fix2int15(a->fy), fix2int15(a->type->radius), BLACK);
-  drawCircle(fix2int15(a->fx), fix2int15(a->fy), fix2int15(a->type->radius)-1, BLACK);
-  drawCircle(fix2int15(a->x), fix2int15(a->y), fix2int15(a->type->radius), color);
-  drawCircle(fix2int15(a->x), fix2int15(a->y), fix2int15(a->type->radius)-1, color);
+  for(int i = 0; i < 5; i++){
+    drawCircle(fix2int15(a->fx), fix2int15(a->fy), fix2int15(a->type->radius)-i, BLACK);
+    drawCircle(fix2int15(a->x), fix2int15(a->y), fix2int15(a->type->radius)-i, color);
+  }
   a->fx = a->x;
   a->fy = a->y;
 }
