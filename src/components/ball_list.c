cfac856ce2288e097993dd3c9582635ca7759f13
iotcat <i@iotcat.me>
2023-12-08 03:55:22 +0000

    Fix initialization of effect ball node

--- src/components/ball_list.c
+++ src/components/ball_list.c
@@ -57,7 +57,7 @@ void initBallNode(fix15 init_x, ball_type* type){
 void initEffectBallNode(fix15 x, fix15 y, fix15 vx, fix15 vy, char color){
   ball a;
   ball_type type = {int2fix15(1), int2fix15(1), color, 0};
-  initBall(&a, x, y, vx, vy, &ball_types[1], false);
+  initBall(&a, x, y, vx, vy, &type, false);
   insertBall(a);
 }
 
